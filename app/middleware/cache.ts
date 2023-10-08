514df8238cc7c383536777df86d1be54bb2ca7d3
Guillaume <jolibois.contact@gmail.com>
2023-10-08 21:17:45 +0200

    :construction: WIP

--- app/middleware/cache.ts
+++ app/middleware/cache.ts
@@ -1,24 +1,27 @@
-// import { NextFunction, Request, Response } from 'express';
-// import logger from '../helpers/logger.js';
-// import Cache from '../service/cache.js';
+import { NextFunction, Request, Response } from 'express';
+import logger from '../helpers/logger.js';
+import CacheService from '../service/cache.js';
 
-// export default (key: string) => async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction,
-// ) => {
-//   if (!key) next();
-//   let paramsKey = key;
-//   if (req.params.id) paramsKey = key + req.params.id;
-//   try {
-//     const cacheValue = await Cache.get(paramsKey);
-//     req.body.cacheKey = paramsKey;
-//     if (req.method === 'GET' && cacheValue) {
-//       return res.status(200).json({ message: 'cached data', data: cacheValue });
-//     }
-//     return next();
-//   } catch (error) {
-//     logger.error(error);
-//     return next();
-//   }
-// };
+export default (key: string) => async (req: Request, res: Response, next: NextFunction) => {
+  // If 'key' is falsy, simply move to the next middleware
+  if (!key) return next();
+
+  const paramsKey = req.params.id ? `${key}${req.params.id}` : key;
+
+  try {
+    const cacheValue = await CacheService.get(paramsKey);
+
+    // Attach the cache key to 'req.body'
+    req.body.cacheKey = paramsKey;
+
+    if (req.method === 'GET' && cacheValue) {
+      return res.status(200).json({ message: 'cached data', data: cacheValue });
+    }
+
+    // If not cached or not a GET request, continue to the next middleware
+    return next();
+  } catch (error) {
+    logger.error(error);
+    return next();
+  }
+};
--- app/service/cache.ts
+++ app/service/cache.ts
@@ -1,20 +1,31 @@
-// import { Redis } from 'ioredis';
+import { Redis } from 'ioredis';
 
-// const redis = new Redis();
+const redis = new Redis({
+  port: Number(process.env.REDIS_PORT), // Redis port
+  host: process.env.REDIS_HOST, // Redis host
+  username: process.env.REDIS_USER, // needs Redis >= 6
+  password: process.env.REDIS_PASSWORD,
+  db: Number(process.env.REDIS_DB), // Defaults to 0
+});
 
-// export default class Cache {
-//   static DEFAULT_EXPIRATION = 300; // 5 minutes
+export default class CacheService {
+  static DEFAULT_EXPIRATION = 300; // 5 minutes
 
-//   static async get(key: string) {
-//     const cacheValue = await redis.get(key);
-//     return cacheValue ? JSON.parse(cacheValue) : null;
-//   }
+  static async get(key: string) {
+    const cacheValue = await redis.get(key);
+    return cacheValue ? JSON.parse(cacheValue) : null;
+  }
 
-//   static async set(key: string, data: any, expiration: number = Cache.DEFAULT_EXPIRATION) {
-//     redis.set(key, JSON.stringify(data), 'EX', expiration);
-//   }
+  static async set(
+    key: string,
+    data: any,
+    expiration: number = CacheService.DEFAULT_EXPIRATION,
+  ) {
+    redis.set(key, JSON.stringify(data), 'EX', expiration);
 
-//   static async del(keys: string[]) {
-//     await redis.del(...keys);
-//   }
-// }
+    // Automatically remove the data from the cache after the specified time.
+    setTimeout(async () => {
+      await redis.del(key);
+    }, this.DEFAULT_EXPIRATION);
+  }
+}
